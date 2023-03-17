60a4d165195babe51a856250c68dcd15599f49da
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-03-17 14:20:10 -0700

    adds func cont lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun (string = 'roller-skate') {
+    return `This Saturday, I want to ${string}!`;
+    }
+    saturdayFun();
+    
+    function mondayWork (string = 'go to the office') {
+        return `This Monday, I will ${string}.`;
+        }
+        saturdayFun();
+    
+    function wrapAdjective(string1 = "*") {
+        return function (string = "special"){
+            return `You are ${string1}${string}${string1}!`
+            }
+        }
+    console.log(wrapAdjective())
